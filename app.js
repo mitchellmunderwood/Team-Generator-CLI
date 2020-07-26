e31253350d606d99d70a35bfa1763c649fde2a4d
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-07-25 21:38:03 -0400

    routed html file using outerPath variable

--- app.js
+++ app.js
@@ -12,12 +12,10 @@ const render = require("./lib/htmlRenderer");
 const ExpandPrompt = require("inquirer/lib/prompts/expand");
 const { ppid } = require("process");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+// create array to hold the member objects
 let team_members = [];
 
+// Prompt to ask the user what type of employee they want to add
 function memberTypePrompt() {
     inquirer.prompt([
         {
@@ -31,6 +29,7 @@ function memberTypePrompt() {
     })
 }
 
+// Prompt to get the three basic questions or every employee
 function EmployeePrompt(type) {
     inquirer.prompt([
         {
@@ -63,6 +62,7 @@ function EmployeePrompt(type) {
     })
 }
 
+// Prompt for manager's office number
 function managerPrompt(type, data_1) {
     inquirer.prompt([
         {
@@ -76,6 +76,7 @@ function managerPrompt(type, data_1) {
     });
 }
 
+// Prompt for the intern's school
 function internPrompt(type, data_1) {
     inquirer.prompt([
         {
@@ -89,6 +90,7 @@ function internPrompt(type, data_1) {
     });
 }
 
+// Prompt to ask for the engineers github
 function engineerPrompt(type, data_1) {
     inquirer.prompt([
         {
@@ -102,6 +104,7 @@ function engineerPrompt(type, data_1) {
     });
 }
 
+// switch statement to create the specific team member object
 function new_member(type, data_1, data_2) {
     let new_object = { type: type, name: data_1.name, id: data_1.id, email: data_1.email };
     switch (type) {
@@ -118,15 +121,21 @@ function new_member(type, data_1, data_2) {
     team_members.push(new_object);
 }
 
+// function to create HTML file based on team member objects
 function endPrompt() {
-    console.log("The Team's data has been recieved and an html is now being generated.");
-    createHTML(team_members);
-    console.log("Your team's file has been generated and is in the 'output' folder.");
+    if (team_members.length > 0) {
+        console.log("The Team's data has been recieved and an html is now being generated.");
+        createHTML(team_members);
+        console.log("Your team's file has been generated and is in the 'output' folder.");
+    } else {
+        console.log("You have not input any team information, no file has been generated.");
+    }
 }
 
+// function to write the team member to an html file
 function createHTML(team) {
     let html_doc = render(createTeam(team));
-    fs.writeFile("./output/team.html", html_doc, function (err) {
+    fs.writeFile(outputPath, html_doc, function (err) {
         if (err) {
             return console.log(err);
         }
@@ -136,11 +145,12 @@ function createHTML(team) {
 
 }
 
+// Prompt to ask whether you want to add a team member or not
 function addMemberPrompt(boolean) {
     result = "start" ? "a" : "another";
     inquirer.prompt([
         {
-            message: `Would you like to add ${result} team member?`,
+            message: `An Engineering Team consists of at least 1 Manager and any number of Engineers and Interns. Would you like to add ${result} team member?`,
             name: "member_add",
             type: "list",
             choices: ["Yes", "No"],
@@ -150,6 +160,7 @@ function addMemberPrompt(boolean) {
     })
 }
 
+// function to create team members from the required classes
 function createTeam(members) {
     return members.map((member) => {
         switch (member.type) {
@@ -163,30 +174,8 @@ function createTeam(members) {
     })
 }
 
+// function all to start the series of prompts
 addMemberPrompt("start");
 
 
 
-
-
-
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
